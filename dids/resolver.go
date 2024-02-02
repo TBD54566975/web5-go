1c40a9ff32557b4c5a61cf5ffad062da0c4ac18d
Mihai Chiorean <mihai.v.chiorean@gmail.com>
2024-02-02 10:23:25 -0800

    default resolver implements interface

--- dids/resolver.go
+++ dids/resolver.go
@@ -8,7 +8,7 @@ import (
 // Resolve resolves the provided DID URI. This function is capable of resolving
 // the DID methods implemented in web5-go
 func Resolve(uri string) (ResolutionResult, error) {
-	return getDefaultResolver().resolve(uri)
+	return getDefaultResolver().Resolve(uri)
 }
 
 var instance *didResolver
@@ -35,7 +35,7 @@ type didResolver struct {
 	resolvers map[string]DIDResolver
 }
 
-func (r *didResolver) resolve(uri string) (ResolutionResult, error) {
+func (r *didResolver) Resolve(uri string) (ResolutionResult, error) {
 	did, err := Parse(uri)
 	if err != nil {
 		return ResolutionResultWithError("invalidDid"), ResolutionError{"invalidDid"}
