3409ec7b7da465f9555dc8561602e3c10e474598
Mihai Chiorean <mihai.v.chiorean@gmail.com>
2024-02-01 17:04:25 -0800

    add more happy path tests

--- dids/did_dht_test.go
+++ dids/did_dht_test.go
@@ -1,6 +1,9 @@
 package dids
 
 import (
+	"encoding/base64"
+	"encoding/hex"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"reflect"
@@ -44,13 +47,20 @@ func makeDNSMessage(answersOpt ...bep44TXTResourceOpt) dnsmessage.Message {
 }
 
 func TestDHTResolve(t *testing.T) {
+	// vector taken from https://github.com/TBD54566975/web5-js/blob/dids-new-crypto/packages/crypto/tests/fixtures/test-vectors/secp256k1/bytes-to-public-key.json
+	publicKeyHexSecp256k1 := "0479be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8"
+	pubKeyBytesSecp256k1, err := hex.DecodeString(publicKeyHexSecp256k1)
+	assert.NoError(t, err)
+	base64EncodedSecp256k := base64.RawURLEncoding.EncodeToString(pubKeyBytesSecp256k1)
 
 	tests := map[string]struct {
+		didURI        string
 		msg           dnsmessage.Message
 		expectedError error
 		assertResult  func(t *testing.T, d *Document)
 	}{
-		"basic did with key": {
+		"did with valid key and no service": {
+			didURI: "did:dht:cwxob5rbhhu3z9x3gfqy6cthqgm6ngrh4k8s615n7pw11czoq4fy",
 			msg: makeDNSMessage(
 				WithDNSRecord("_did.", "vm=k0;auth=k0;asm=k0;inv=k0;del=k0"),
 				WithDNSRecord("_k0._did.", "id=0;t=0;k=YCcHYL2sYNPDlKaALcEmll2HHyT968M4UWbr-9CFGWE"),
@@ -60,6 +70,37 @@ func TestDHTResolve(t *testing.T) {
 				assert.False(t, d == nil, "Expected non nil document")
 				assert.NotZero[string](t, d.ID, "Expected DID Document ID to be initialized")
 				assert.NotZero[[]VerificationMethod](t, d.VerificationMethod, "Expected at least 1 verification method")
+			},
+		},
+		"did with multiple valid keys and no service - out of order verification methods": {
+			didURI: "did:dht:cwxob5rbhhu3z9x3gfqy6cthqgm6ngrh4k8s615n7pw11czoq4fy",
+			msg: makeDNSMessage(
+				WithDNSRecord("_did.", "vm=k0,k1,k2;auth=k0;asm=k1;inv=k2;del=k0"),
+				WithDNSRecord("_k0._did.", "id=0;t=0;k=YCcHYL2sYNPDlKaALcEmll2HHyT968M4UWbr-9CFGWE"),
+				WithDNSRecord("_k2._did.", fmt.Sprintf("id=2;t=1;k=%s", base64EncodedSecp256k)),
+				WithDNSRecord("_k1._did.", fmt.Sprintf("id=1;t=2;k=%s", base64EncodedSecp256k)),
+			),
+			expectedError: nil,
+			assertResult: func(t *testing.T, d *Document) {
+				assert.False(t, d == nil, "Expected non nil document")
+				assert.NotZero[string](t, d.ID, "Expected DID Document ID to be initialized")
+				assert.Equal[int](t, 3, len(d.VerificationMethod), "Expected 3 verification methods")
+			},
+		},
+		"did with key controller and services": {
+			didURI: "did:dht:cwxob5rbhhu3z9x3gfqy6cthqgm6ngrh4k8s615n7pw11czoq4fy",
+			msg: makeDNSMessage(
+				WithDNSRecord("_did.", "vm=k0;auth=k0;asm=k0;inv=k0;del=k0;srv=s0,s1"),
+				WithDNSRecord("_k0._did.", "id=0;t=0;k=YCcHYL2sYNPDlKaALcEmll2HHyT968M4UWbr-9CFGWE"),
+				WithDNSRecord("_s0._did.", "id=domain;t=LinkedDomains;se=foo.com"),
+				WithDNSRecord("_s1._did.", "id=dwn;t=DecentralizedWebNode;se=https://dwn.tbddev.org/dwn5"),
+			),
+			expectedError: nil,
+			assertResult: func(t *testing.T, d *Document) {
+				assert.False(t, d == nil, "Expected non nil document")
+				assert.NotZero[string](t, d.ID, "Expected DID Document ID to be initialized")
+				assert.NotZero[[]VerificationMethod](t, d.VerificationMethod, "Expected at least 1 verification method")
+				assert.Equal[int](t, 2, len(d.Service), "Expected 2 services")
 			},
 		},
 	}
@@ -75,7 +116,7 @@ func TestDHTResolve(t *testing.T) {
 			}))
 			defer ts.Close()
 
-			document, err := ResolveDIDDHT("did:dht:cwxob5rbhhu3z9x3gfqy6cthqgm6ngrh4k8s615n7pw11czoq4fy", ts.URL, http.DefaultClient)
+			document, err := ResolveDIDDHT(test.didURI, ts.URL, http.DefaultClient)
 
 			if test.expectedError != nil {
 				assert.Error(t, test.expectedError, err)
