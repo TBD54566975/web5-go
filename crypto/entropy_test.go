b08cc0027cff25de51f4e306d81b0eb6f223e5c8
Kendall Weihe <kendallw@squareup.com>
2024-02-20 14:41:21 -0500

    Remove unnecessary test overhead

--- crypto/entropy_test.go
+++ crypto/entropy_test.go
@@ -1,6 +1,7 @@
 package crypto_test
 
 import (
+	"encoding/hex"
 	"testing"
 
 	"github.com/alecthomas/assert/v2"
@@ -25,16 +26,10 @@ func Test_GenerateEntropy_InvalidSize(t *testing.T) {
 }
 
 func Test_GenerateNonce(t *testing.T) {
-	nonceMap := make(map[string]bool)
+	nonce, err := crypto.GenerateNonce()
+	assert.NoError(t, err)
+	assert.Equal(t, 32, len(nonce))
 
-	for i := 0; i < 1000000; i++ {
-		nonce, err := crypto.GenerateNonce()
-		assert.NoError(t, err)
-		assert.Equal(t, 32, len(nonce))
-
-		nonceMap[nonce] = true
-	}
-
-	// Assert that we have 1000000 unique nonces
-	assert.Equal(t, 1000000, len(nonceMap))
+	_, err = hex.DecodeString(nonce)
+	assert.NoError(t, err)
 }
