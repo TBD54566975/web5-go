d582c3af02601e6075382c2381c44b02b4cb2c04
Moe Jangda <moe@tbd.email>
2024-02-21 03:23:50 -0600

    narrow generic type using `CredentialSubject` interface

--- vc/vc_test.go
+++ vc/vc_test.go
@@ -2,24 +2,27 @@ package vc_test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/alecthomas/assert/v2"
-	"github.com/tbd54566975/web5-go/dids/didjwk"
 	"github.com/tbd54566975/web5-go/vc"
 )
 
-func TestSomething(t *testing.T) {
-	type YoloClaims struct {
-		Hello string `json:"hello"`
-	}
+func TestCreate_Defaults(t *testing.T) {
+	cred := vc.Create(vc.Claims{"id": "1234"})
 
-	claims := YoloClaims{Hello: "world"}
-	cred := vc.Create(claims)
+	assert.Equal(t, 1, len(cred.Context))
+	assert.Equal(t, vc.BaseContext, cred.Context[0])
 
-	bearerDID, err := didjwk.Create()
-	if err != nil {
-		assert.NoError(t, err)
-	}
+	assert.Equal(t, 1, len(cred.Type))
+	assert.Equal(t, vc.BaseType, cred.Type[0])
 
-	cred.SignJWT(bearerDID)
+	assert.Contains(t, cred.ID, "urn:vc:uuid:")
+
+	assert.NotZero(t, cred.IssuanceDate)
+
+	_, err := time.Parse(time.RFC3339, cred.IssuanceDate)
+	assert.NoError(t, err)
+
+	assert.Equal(t, "1234", cred.CredentialSubject["id"])
 }
