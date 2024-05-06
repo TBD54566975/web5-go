366556dd004e10200140c47b8ec9567f5674053b
Moe Jangda <moe@tbd.email>
2024-05-06 14:16:29 -0500

    smol changes (#144)
    * change filename to follow go convention
    
    Co-Authored-By: Jiyoon Koo <jiyoon@tbd.email>
    
    * replace `interface{}` with `any`
    
    Co-Authored-By: Jiyoon Koo <jiyoon@tbd.email>
    
    * use map as a set instead of deduping a list
    
    Co-Authored-By: Jiyoon Koo <jiyoon@tbd.email>
    
    * move `SelectCredentials` to pd receiver method
    
    Co-Authored-By: Jiyoon Koo <jiyoon@tbd.email>
    
    * return early if an input descriptor is not satisfied
    
    * lint
    
    * add `required bool` to `JsonSchema.AddProperty`
    
    Co-Authored-By: Jiyoon Koo <jiyoon@tbd.email>
    
    ---------
    
    Co-authored-by: Jiyoon Koo <jiyoon@tbd.email>

--- pexv2/pd.go
+++ pexv2/pd.go
@@ -1,5 +1,17 @@
 package pexv2
 
+import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"strconv"
+	"time"
+
+	"github.com/PaesslerAG/jsonpath"
+	"github.com/santhosh-tekuri/jsonschema/v5"
+	"github.com/tbd54566975/web5-go/vc"
+)
+
 // PresentationDefinition represents a DIF Presentation Definition defined [here].
 // Presentation Definitions are objects that articulate what proofs a Verifier requires
 //
@@ -20,6 +32,98 @@ type InputDescriptor struct {
 	Name        string      `json:"name,omitempty"`
 	Purpose     string      `json:"purpose,omitempty"`
 	Constraints Constraints `json:"constraints"`
+}
+
+type tokenizedField struct {
+	token string
+	path  string
+}
+
+// SelectCredentials selects vcJWTs based on the constraints defined in the input descriptor
+func (ind InputDescriptor) SelectCredentials(vcJWTs []string) ([]string, error) {
+	jsonSchema := JSONSchema{
+		Schema:     "http://json-schema.org/draft-07/schema#",
+		Type:       "object",
+		Properties: make(map[string]Filter, len(ind.Constraints.Fields)),
+		Required:   make([]string, 0, len(ind.Constraints.Fields)),
+	}
+
+	tokenizedFields := make([]tokenizedField, 0, len(ind.Constraints.Fields))
+	tokens := make([]string, len(ind.Constraints.Fields))
+
+	for i, field := range ind.Constraints.Fields {
+		token := strconv.FormatInt(time.Now().UnixNano(), 10)
+		tokens[i] = token
+
+		for _, path := range field.Path {
+			tf := tokenizedField{token: token, path: path}
+			tokenizedFields = append(tokenizedFields, tf)
+		}
+
+		if field.Filter != nil {
+			jsonSchema.AddProperty(token, *field.Filter, true)
+		}
+	}
+
+	sch, err := json.Marshal(jsonSchema)
+	if err != nil {
+		return nil, fmt.Errorf("error marshalling schema: %w", err)
+	}
+
+	schema, err := jsonschema.CompileString(ind.ID, string(sch))
+	if err != nil {
+		return nil, fmt.Errorf("error compiling schema: %w", err)
+	}
+
+	matched := make([]string, 0, len(vcJWTs))
+
+	for _, vcJWT := range vcJWTs {
+		tokensFound := make(map[string]bool, len(tokenizedFields))
+
+		decoded, err := vc.Decode[vc.Claims](vcJWT)
+		if err != nil {
+			continue
+		}
+
+		var jwtPayload map[string]any
+		payload, err := base64.RawURLEncoding.DecodeString(decoded.JWT.Parts[1])
+		if err != nil {
+			continue
+		}
+
+		if err := json.Unmarshal(payload, &jwtPayload); err != nil {
+			continue
+		}
+
+		selectionCandidate := make(map[string]any)
+		for _, tf := range tokenizedFields {
+			if ok := tokensFound[tf.token]; ok {
+				continue
+			}
+
+			value, err := jsonpath.Get(tf.path, jwtPayload)
+			if err != nil {
+				continue
+			}
+
+			if value != nil {
+				selectionCandidate[tf.token] = value
+				tokensFound[tf.token] = true
+			}
+		}
+
+		if len(selectionCandidate) != len(tokens) {
+			continue
+		}
+
+		if err := schema.Validate(selectionCandidate); err != nil {
+			continue
+		}
+
+		matched = append(matched, vcJWT)
+	}
+
+	return matched, nil
 }
 
 // Constraints contains the requirements for a given Input Descriptor.
@@ -53,4 +157,21 @@ type Filter struct {
 	Pattern  string  `json:"pattern,omitempty"`
 	Const    string  `json:"const,omitempty"`
 	Contains *Filter `json:"contains,omitempty"`
+}
+
+// JSONSchema represents a minimal JSON Schema
+type JSONSchema struct {
+	Schema     string            `json:"$schema"`
+	Type       string            `json:"type"`
+	Properties map[string]Filter `json:"properties"`
+	Required   []string          `json:"required"`
+}
+
+// AddProperty adds the provided Filter with the provided name to the JsonSchema
+func (j *JSONSchema) AddProperty(name string, value Filter, required bool) {
+	j.Properties[name] = value
+
+	if required {
+		j.Required = append(j.Required, name)
+	}
 }
